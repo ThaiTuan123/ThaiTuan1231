e985cbb585fc458ff6d332c9bb9cb82344535c9a
ThaiTuan123 <53283080+ThaiTuan123@users.noreply.github.com>
2020-03-31 09:06:25 +0700

    Add files via upload

---
+++ PersonsInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp
+{
+    class PersonsInfo
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public PersonsInfo(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+
+        }
+            public int Age
+    {
+            get { return this.age; }
+            set { this.age = value; }
+    }
+
+
+        public override string ToString()
+        {
+            return $"{this.firstName}{ this.lastName} is { this.age} years old" ;
+        }
+
+    }
+}
